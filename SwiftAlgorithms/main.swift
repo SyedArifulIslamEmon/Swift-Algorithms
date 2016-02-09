aa2afd780f8ef9e4462397cf450902e88be3771d
khoiln <khoi.geeky@gmail.com>
2016-02-10 00:15:05 +0700

    Initial Commit

---
+++ SwiftAlgorithms/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SwiftAlgorithms
+//
+//  Created by baga on 2/10/16.
+//  Copyright © 2016 Khoi Lai. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
